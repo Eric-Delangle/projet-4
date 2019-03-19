92604e616f7e53b1fa792272544c43e1a04d1aed
Eric-Delangle <polvu@hotmail.fr>
2019-03-19 08:55:31 +0100

    Factorisation en MVC et commencement de poo le 19/03

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors','on');
+error_reporting(E_ALL);
+
+class Myautoload {
+
+    public static function start() {
+
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        $host = $_SERVER['HTTP_HOST'];
+
+        define('HOST', 'http://'.$host.'/ecrivain/');
+        define('ROOT', $root.'/ecrivain/');
+
+        define('CONTROLER', ROOT.'controler/');
+        define('VIEW', ROOT.'view/');
+        define('MODEL', ROOT.'model/');
+        define('CLASSES', ROOT.'classes/');
+
+        define('ASSETS', HOST.'assets/');
+    }
+
+    public static function autoload($class) {
+        if(file_exists(MODEL.$class.'.php')) {
+            include_once(MODEL.$class.'.php');
+        } else if (file_exists(CLASSES.$class.'.php')) {
+            include_once(CLASSES.$class.'.php');
+        }else if (file_exists(CONTROLER.$class.'.php')) {
+            include_once(CONTROLER.$class.'.php');
+        }
+    }
+}
+
+
+
