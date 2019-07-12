d6f3d86810277634a9b8e7a7eb1fc2c4fcdc58a9
Eric-Delangle <polvu@hotmail.fr>
2019-07-12 19:13:53 +0200

    Finalisation de l'option suivant/precedent le 12/07

--- view/frontend/oneChapView.php
+++ view/frontend/oneChapView.php
@@ -17,8 +17,15 @@ include(VIEW.'nav.php');
                     <?= $data['date_parution_fr'] ?>
                  </p>
             </div>
-        <?php
-
+<div id='precsuiv'>
+    <form method="post" action="precedent?number=<?=$_GET['number']?>">
+        <input type="submit" class="liens_h1" value="precedent" name="precedent"/>
+    </form>
+    <form method="post" action="suivant?number=<?=$_GET['number']?>">
+        <input type="submit" class="liens_h1" value="suivant" name= "suivant" />
+    </form>
+</div>
+<?php
 showCom($_GET['id']); // j'appelle ma fonction pour afficher les commentaires
 $readChapter->closeCursor();
 ?>
