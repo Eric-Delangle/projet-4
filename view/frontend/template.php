dfc83f9c1de31a07785306727788b8a7ab0b812c
Eric-Delangle <polvu@hotmail.fr>
2019-07-14 10:50:35 +0200

    Validation w3c le 14/07

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -6,7 +6,6 @@
         <title><?= $title ?>Billet simple pour l'Alaska</title>
         <link href="assets/css/style.css" rel="stylesheet" /> 
         <link rel="shortcut icon" type="image/png" href="<?php echo ASSETS;?>images/baleine3favicon.png"/>
-        <link rel="stylesheet" type="text/css" href="assets/css/normalize.css" />
     </head>
         
     <body>
@@ -19,7 +18,6 @@
         <script>document.getElementById('connecter').style.display = 'none';</script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="assets/js/animChapitres.js"></script>
-        <script src="assets/js/modernizr.custom.js"></script>
         <script src = "https://www.google.com/recaptcha/api.js"></script>
     </body>
 </html>
